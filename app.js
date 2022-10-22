64a7d5147348992634fa63e9cc102b09cb7e484d
Brian Etaghene <joveee05@gmail.com>
2022-10-22 22:34:02 +0100

    Updated

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use("/", indexRouter);
-app.use('/api/v1/users', userRouter);
+app.use('/', userRouter);
 
 module.exports = app;
